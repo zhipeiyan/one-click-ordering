b2718ed06a53bdd1e85962715984c41fc41c4d8a
Zhipei Yan <yanzp@tamu.edu>
2021-03-26 19:35:10 -0500

    Update the settings for uueat since they changed the ordering for the combination of restaurants and groceries.

--- OneClickOrdering.py
+++ OneClickOrdering.py
@@ -11,15 +11,11 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
 # configuration of the order file
-orders = r'C:\path\to\your.xlsx'
+order_file = r'C:\path\to\your.xlsx'
 # sheet name in your xlsx
 date = 'Jan 1 Retailer'
-columns_per_person = 3
-skip_head_lines = 2
-skip_rear_lines = 9
-# modify the website to any shopping websites powered by Ecwid you want to use. Here are two examples.
-website = 'run4uhome'
-# website = 'uueat'
+retailer = 'uueat'
+# retailer = 'run4uhome'
 
 # configuration of your system
 browser = 'Edge'
@@ -39,26 +35,38 @@ def init_browser_driver(browser_name, driver_path):
         sys.exit('Implement the browser driver initialization of your choice here.')
 
 
-def read_sheet(file, sheet):
+def init_website_settings(retailer_name):
+    # columns_per_person, skip_head_lines, skip_rear_lines, websites
+    # add any retailer powered by Ecwid you want to use to websites
+    if retailer_name == 'run4uhome':
+        return 3, 2, 7, ['run4uhome']
+    elif retailer_name == 'uueat':
+        return 4, 2, 14, ['uueat', 'uucart']
+    else:
+        print('Error! None supported retailer', retailer_name)
+        return 0, 0, 0, []
+
+
+def read_sheet(file, sheet, cols_per_person, skip_head, skip_rear):
     ws = xlrd.open_workbook(file).sheet_by_name(sheet)
     # xlrd can't read hyperlinks from .xlsx files, use openpyxl
     read_links = openpyxl.load_workbook(file)[sheet]
     items = {}
-    for person in range(ws.ncols // columns_per_person):
-        for row in range(skip_head_lines, ws.nrows - skip_rear_lines):
+    for person in range(ws.ncols // cols_per_person):
+        for row in range(skip_head, ws.nrows - skip_rear):
             # In xlrd, type 2 means number
-            if ws.cell(row, columns_per_person * person + 1).ctype == 2:
+            if ws.cell(row, cols_per_person * person + cols_per_person - 2).ctype == 2:
                 # openpyxl indexed from 1, while xlrd indexed from 0
-                link = read_links.cell(row=row + 1, column=columns_per_person * person + 1).hyperlink
-                count = ws.cell(row, columns_per_person * person + 1).value
+                link = read_links.cell(row=row + 1, column=cols_per_person * person + 1).hyperlink
+                count = ws.cell(row, cols_per_person * person + cols_per_person - 2).value
                 if link is not None:
                     if link.target in items:
                         items[link.target] += count
                     else:
                         items[link.target] = count
                 else:
-                    print('Error! No url provided for the item:', ws.cell(0, columns_per_person * person).value,
-                          ws.cell(row, columns_per_person * person).value)
+                    print('Error! No url provided for the item:', ws.cell(0, cols_per_person * person).value,
+                          ws.cell(row, cols_per_person * person).value)
     return items
 
 
@@ -74,12 +82,20 @@ if __name__ == '__main__':
     driver = init_browser_driver(browser, browser_driver_path)
     # driver.maximize_window()
     wait = WebDriverWait(driver, 10)
-    order = read_sheet(orders, date)
-    for item, quantity in order.items():
+
+    columns_per_person, skip_head_lines, skip_rear_lines, websites = init_website_settings(retailer)
+    orders = read_sheet(order_file, date, columns_per_person, skip_head_lines, skip_rear_lines)
+
+    for item, quantity in orders.items():
         if quantity.is_integer():
             add_to_bag(item, int(quantity))
         else:
             print('Error! None integer quantity of an item in the order:', item)
-    driver.get('https://www.' + website + '.com/cart')
+
+    if len(websites):
+        driver.get('https://www.' + websites[0] + '.com/cart')
+    for website in websites[1:]:
+        driver.execute_script('window.open("http://www.' + website + '.com/cart","_blank");')
+
     input('Done. Press enter to exit.')
     driver.quit()
